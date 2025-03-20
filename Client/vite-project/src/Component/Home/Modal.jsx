a96f9590d1c3659b5951f1ad12bc2532a4ab7772
Kenil-M-patel <kenilp1911@gamil.com>
2025-03-20 22:11:04 +0530

    done

--- Client/vite-project/src/Component/Home/Modal.jsx
+++ Client/vite-project/src/Component/Home/Modal.jsx
@@ -1,133 +1,136 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch} from 'react-redux'
-import { updateProfileField, updateHobbies, reset} from '../../Slice/FormSlice';
-import {ReadFromDB} from '../../Slice/UserSlice.js'
-import {toggleModal} from '../../Slice/modal.js';
-import {total, resetQuery} from '../../Slice/Queries'
-import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux'
+import { updateProfileField, updateHobbies, reset } from '../../Slice/FormSlice';
+import { ReadFromDB } from '../../Slice/UserSlice.js'
+import { toggleModal } from '../../Slice/modal.js';
+import { total, resetQuery } from '../../Slice/Queries'
+import axiosInstance from '../../axios/axios'
 
 
 const ModalComponent = () => {
-  
+
   const formData = useSelector(state => state.form)
-  const dispatch =  useDispatch()
+  const dispatch = useDispatch()
   const isModalOpen = useSelector(state => state.modal)
   const [selectedFile, setSelectedFile] = useState(null);
-  const quries =  useSelector(state => state.queries)
+  const quries = useSelector(state => state.queries)
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target; 
-    dispatch(updateProfileField({ name, value })); 
+    const { name, value } = e.target;
+    dispatch(updateProfileField({ name, value }));
   }
 
   const handleHobbiesChange = (e) => {
     const { value, checked } = e.target;
-    dispatch(updateHobbies({ hobby: value, checked })); 
+    dispatch(updateHobbies({ hobby: value, checked }));
   };
 
   const handlePhotoChange = (e) => {
     const file = e.target.files[0];
     setSelectedFile(file); // Keep the file in local state
   };
- 
 
-  const handleSubmit = async(e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
 
-     
+
+
     const formDataToSubmit = new FormData();
     formDataToSubmit.append('FirstName', formData.FirstName);
     formDataToSubmit.append('LastName', formData.LastName);
     formDataToSubmit.append('Email', formData.Email);
     formDataToSubmit.append('MobileNumber', formData.MobileNumber);
-    formDataToSubmit.append('Hobbies', JSON.stringify(formData.Hobbies)); 
+    formDataToSubmit.append('Hobbies', JSON.stringify(formData.Hobbies));
     formDataToSubmit.append('Gender', formData.Gender);
     if (formData._id) {
-        formDataToSubmit.append('_id', formData._id);
+      formDataToSubmit.append('_id', formData._id);
     }
     if (selectedFile) {
       formDataToSubmit.append('file', selectedFile);
     }
-  if (formDataToSubmit._id === '') {
+    if (formDataToSubmit._id === '') {
       delete formDataToSubmit._id;
-  }
-  console.log(formData)
-  try {
-      if (formData._id) {
-          await axios.put(`http://localhost:5000/User`, formDataToSubmit,{
-              headers: {
-                  'Content-Type': 'multipart/form-data',
-              },
-          });
-          dispatch(toggleModal());
-          dispatch(reset());
-          const queryString = new URLSearchParams(quries).toString();
-          const response = await axios.get(`http://localhost:5000/User?${queryString}`);
-                
-                        const formattedData = response.data.user.map((user) => {
-                          return {
-                            _id: user._id,
-                            FirstName: user.FirstName,
-                            LastName: user.LastName,
-                            Email: user.Email,
-                            MobileNumber: user.MobileNumber,
-                            Hobbies: user.Hobbies,
-                            Gender: user.Gender,
-                            filename: user.url,
-                          };
-                        }); 
-          dispatch(resetQuery())
-          dispatch(total(response.data.totalPages))  
-          dispatch(ReadFromDB(formattedData));
-      } else {
-          console.log(formDataToSubmit);
-          await axios.post(`http://localhost:5000/User`, formDataToSubmit, {
-              headers: {
-                  'Content-Type': 'multipart/form-data',
-              },
-          });
-          dispatch(toggleModal());
-          dispatch(reset());
-          const queryString = new URLSearchParams(quries).toString();
-          const response = await axios.get(`http://localhost:5000/User?${queryString}`);
-                
-                        const formattedData = response.data.user.map((user) => {
-                          return {
-                            _id: user._id,
-                            FirstName: user.FirstName,
-                            LastName: user.LastName,
-                            Email: user.Email,
-                            MobileNumber: user.MobileNumber,
-                            Hobbies: user.Hobbies,
-                            Gender: user.Gender,
-                            filename: user.url,
-                          };
-                        });
-          dispatch(resetQuery())
-          dispatch(total(response.data.totalPages))             
-          dispatch(ReadFromDB(formattedData));
-      }
-  } catch (error) {
-      if (error.response.data.flag === 1) {
-          alert("Email already exists");
-      }
-      if (error.response.data.flag === 2) {
-          alert("Valid Mobile Number is required");
-      }
-      console.error("Error submitting form data:", error.response.data);
-  }
-    
-  };
-
-
+    }
+    console.log(formData)
+    try {
+      if (formData._id)
+      {
+        await axiosInstance.put('/User', formDataToSubmit, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          }
+        });
   
+      dispatch(toggleModal());
+      dispatch(reset());
+      const queryString = new URLSearchParams(quries).toString();
 
-  return (
-    <div>
+       const response =  await axiosInstance.put(`/User?${queryString}`);
+        const formattedData = response.data.user.map((user) => {
+          return {
+            _id: user._id,
+            FirstName: user.FirstName,
+            LastName: user.LastName,
+            Email: user.Email,
+            MobileNumber: user.MobileNumber,
+            Hobbies: user.Hobbies,
+            Gender: user.Gender,
+            filename: user.url,
+          };
+        });
+        dispatch(resetQuery())
+        dispatch(total(response.data.totalPages))
+        dispatch(ReadFromDB(formattedData));
+    } 
+    else {
+  
+      await axiosInstance.post('/User', formDataToSubmit, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        }
+      });
+      dispatch(toggleModal());
+      dispatch(reset());
+      const queryString = new URLSearchParams(quries).toString();
+      const response =  await axiosInstance.put(`/User?${queryString}`);
+
+      const formattedData = response.data.user.map((user) => {
+        return {
+          _id: user._id,
+          FirstName: user.FirstName,
+          LastName: user.LastName,
+          Email: user.Email,
+          MobileNumber: user.MobileNumber,
+          Hobbies: user.Hobbies,
+          Gender: user.Gender,
+          filename: user.url,
+        };
+      });
+      dispatch(resetQuery())
+      dispatch(total(response.data.totalPages))
+      dispatch(ReadFromDB(formattedData));
+    }
+  } catch (error) {
+    if (error.response.data.flag === 1) {
+      alert("Email already exists");
+    }
+    if (error.response.data.flag === 2) {
+      alert("Valid Mobile Number is required");
+    }
+    console.error("Error submitting form data:", error.response.data);
+  }
+
+};
+
+
+
+
+return (
+  <div>
 
     <button
-      onClick={()=>{dispatch(toggleModal())}}
+      onClick={() => { dispatch(toggleModal()) }}
       className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
     >
       Add user
@@ -147,17 +150,17 @@ const ModalComponent = () => {
               </h3>
             </div>
 
-            <form className="p-4 md:p-5"  onSubmit={handleSubmit}>
+            <form className="p-4 md:p-5" onSubmit={handleSubmit}>
               <div className="grid gap-4 mb-4 grid-cols-2">
                 <div className="col-span-2">
-                <input
+                  <input
                     type="text"
                     name="_id"
                     value={formData._id}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                   
+
                   />
-                  
+
                   <label htmlFor="FirstName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     First Name
                   </label>
@@ -222,70 +225,70 @@ const ModalComponent = () => {
                 </div>
 
                 <div className="col-span-2">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Hobbies
+                  </label>
+                  <div className="flex items-center space-x-4">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                      Hobbies
+                      <input
+                        type="checkbox"
+                        value="Sports"
+                        checked={formData.Hobbies.includes('Sports') || false}
+                        onChange={handleHobbiesChange}
+                      />
+                      Sports
                     </label>
-                    <div className="flex items-center space-x-4">
-                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        <input
-                          type="checkbox"
-                          value="Sports"
-                          checked={formData.Hobbies.includes('Sports') || false}
-                          onChange={handleHobbiesChange}
-                        />
-                        Sports
-                      </label>
-                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        <input
-                          type="checkbox"
-                          value="Art"
-                          checked={formData.Hobbies.includes('Art') || false}
-                          onChange={handleHobbiesChange}
-                        />
-                        Art
-                      </label>
-                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        <input
-                          type="checkbox"
-                          value="Game"
-                          checked={formData.Hobbies.includes('Game') || false}
-                          onChange={handleHobbiesChange}
-                        />
-                        Game
-                      </label>
-                    </div>
-                  </div>
-
-                  <div className="col-span-2">
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                      Gender
+                      <input
+                        type="checkbox"
+                        value="Art"
+                        checked={formData.Hobbies.includes('Art') || false}
+                        onChange={handleHobbiesChange}
+                      />
+                      Art
                     </label>
-                    <div className="flex items-center space-x-4">
-                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        <input
-                          type="radio"
-                          name="Gender"
-                          value="male"
-                          checked={formData.Gender === 'male'}
-                          onChange={handleInputChange}
-                        />
-                        Male
-                      </label>
-                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        <input
-                          type="radio"
-                          name="Gender"
-                          value="female"
-                          checked={formData.Gender === 'female'}
-                          onChange={handleInputChange}
-                        />
-                        Female
-                      </label>
-                      
-                    </div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      <input
+                        type="checkbox"
+                        value="Game"
+                        checked={formData.Hobbies.includes('Game') || false}
+                        onChange={handleHobbiesChange}
+                      />
+                      Game
+                    </label>
                   </div>
+                </div>
 
-                  <div className="col-span-2">
+                <div className="col-span-2">
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Gender
+                  </label>
+                  <div className="flex items-center space-x-4">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      <input
+                        type="radio"
+                        name="Gender"
+                        value="male"
+                        checked={formData.Gender === 'male'}
+                        onChange={handleInputChange}
+                      />
+                      Male
+                    </label>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      <input
+                        type="radio"
+                        name="Gender"
+                        value="female"
+                        checked={formData.Gender === 'female'}
+                        onChange={handleInputChange}
+                      />
+                      Female
+                    </label>
+
+                  </div>
+                </div>
+
+                <div className="col-span-2">
                   <label htmlFor="LastName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Photo
                   </label>
@@ -298,11 +301,11 @@ const ModalComponent = () => {
                     onChange={handlePhotoChange}
                   />
                 </div>
-                  </div>
+              </div>
 
               <button
                 type="submit"
-               
+
                 className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -312,27 +315,27 @@ const ModalComponent = () => {
               </button>
 
               <button
-              type="button"
-              onClick={() => {
-                dispatch(toggleModal());
-                dispatch(reset());
-              }}
-              className="ml-2 text-white inline-flex items-center bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
-            >
-              <svg
-                className="me-1 -ms-1 w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+                type="button"
+                onClick={() => {
+                  dispatch(toggleModal());
+                  dispatch(reset());
+                }}
+                className="ml-2 text-white inline-flex items-center bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              Close
-            </button>
+                <svg
+                  className="me-1 -ms-1 w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                Close
+              </button>
 
             </form>
           </div>
@@ -340,7 +343,7 @@ const ModalComponent = () => {
       </div>
     )}
   </div>
-  );
+);
 };
 
 export default ModalComponent;
